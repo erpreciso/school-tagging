bec811bfda75a06b53f185c3b418a3ab7acce731
erpreciso <trepreciso@gmail.com>
2014-03-15 16:31:13 +0100

    added all html templates

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+# coding: utf-8
+# [school-tagging] webapp
 
 import webapp2
+import jinja2
+import os
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+	template_dir = os.path.join(os.path.dirname(__file__), 'pages')
+	jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+		autoescape = True)
+	def writeout(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+	
+	def readout(self,param):
+		return self.request.get(param)
+		
+	def render_str(self, template, **params):
+		return self.jinja_env.get_template(template).render(params)
+		
+	def render_page(self, template, **kw):
+		self.writeout(self.render_str(template, **kw))
+
+class HomePageHandler(MainHandler):
+	def get(self):
+		self.render_page("content.html")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', HomePageHandler)
 ], debug=True)
