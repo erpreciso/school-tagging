53e3fd94c90489ff53440a871cf21d7b6909605e
erpreciso <trepreciso@gmail.com>
2014-03-18 08:46:47 +0100

    added different templates student vs teachers

--- main.py
+++ main.py
@@ -8,26 +8,54 @@ import re
 import random
 import string
 import hashlib
+import logging
 
 DEBUG = True
 #~ Debug settings
 #~ --------------
 #~ in check_cookie: returns True even if there are no cookie
 #~ USER_CACHE global variable always contains a student login
+#~ in Logout class: doesn't clear the cache
 #~ -----------------------------------------------------------
 
 USER_CACHE = {
 	'hashpassword': '7d69e7ad3f56a8cfa6b35450ac1903b64557c1d9223393e6b863c9cc86bd26db|Okzmj',
-	'user': u'rrr',
+	'username': u'rrr',
 	'role': u'students',
 	}
 
-USERS = {"teachers": [], "students": []}
+USERS = {
+	"teachers": [
+		{
+		'hashpassword': 'cbf37cbdaa4b4bf387264b1b980f0b8e0345aeeb47bb56678477bd70ad71ee47|BOZCt',
+		'role': u'teachers',
+		'username': u'ttt'
+		}
+	],
+	"students": [
+		{
+		'hashpassword': '7d69e7ad3f56a8cfa6b35450ac1903b64557c1d9223393e6b863c9cc86bd26db|Okzmj',
+		'username': u'rrr',
+		'role': u'students',
+		}
+	]
+	}
+
+def get_user_role():
+	if USER_CACHE["role"] == "students":
+		return "student"
+	elif USER_CACHE["role"] == "teachers":
+		return "teacher"
+	else:
+		raise Exception("No known role")
+
+def get_user_name():
+	return USER_CACHE["username"]
 
 def add_user_to_cache(role, username, password):
 	global USER_CACHE
 	USER_CACHE["role"] = role
-	USER_CACHE["user"] = username
+	USER_CACHE["username"] = username
 	USER_CACHE["hashpassword"] = password
 	
 def all_usernames():
@@ -39,25 +67,26 @@ def add_user_to_database(role, username, password, id="default"):
 	user = {
 			"id": id,
 			"username": username,
-			"password": password,
+			"hashpassword": password,
 			}
 	USERS[role].append(user)
 
 def get_user_password(role, username):
-	for k in USERS[role]:
-		if k["username"] == username:
-			return k["password"]
+	logging.info(role)
+	for user in USERS[role]:
+		if user["username"] == username:
+			return user["hashpassword"]
 	return False
 
 def check_cookie(c):
 	if DEBUG:
 		return True
-	if 'user' in USER_CACHE.keys():
+	if 'username' in USER_CACHE.keys():
 		if c and '|' in c:
 			if '|' in c[c.find('|') + 1:]:
 				username = c.split('|')[0]
 				user_pswsalt = '%s|%s' % (c.split('|')[1], c.split('|')[2])
-				if username == USER_CACHE['user'] and user_pswsalt == USER_CACHE['hashpassword']:
+				if username == USER_CACHE['username'] and user_pswsalt == USER_CACHE['hashpassword']:
 					return True
 	global USER_CACHE
 	USER_CACHE.clear()
@@ -204,18 +233,25 @@ class LogoutPageHandler(MainHandler):
 	
 	def get(self):
 		clear_my_cookie(self)
-		USER_CACHE.clear()
+		if not DEBUG:
+			USER_CACHE.clear()
 		self.redirect("/login")
 
 class WelcomePageHandler(MainHandler):
 	def get(self):
 		cookie = self.request.cookies.get("schooltagging")
 		if check_cookie(cookie):
-			self.render_page(
-					"welcome.html",
-					username=USER_CACHE["user"],
-					cache = USER_CACHE,
-					)
+			if get_user_role() == "student":
+				self.render_page(
+						"student.html",
+						username=get_user_name(),
+						)
+			elif get_user_role() == "teacher":
+				self.render_page(
+						"teacher.html",
+						username=get_user_name(),
+						cache=USER_CACHE,
+						)
 		else:
 			self.redirect("/login")
 
