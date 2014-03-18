f049ce4b3050b3401ddc11a4ee37c88de04b9592
erpreciso <trepreciso@gmail.com>
2014-03-18 05:48:39 +0100

    completed signup algorithm

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import webapp2
 import jinja2
 import os
 import re
-from google.appengine.ext import ndb
+import random
+import string
+import hashlib
 
 USERS = {
 	"teachers": [
@@ -28,6 +30,28 @@ USERS = {
 		},
 	],
 }
+
+def all_usernames():
+	a = [x["username"] for x in USERS["teachers"]]
+	b = [x["username"] for x in USERS["students"]]
+	return a + b
+
+def add_user(role, username, password, id="default"):
+	a = {
+				"id": id,
+				"username": username,
+				"password": password,
+			}
+	USERS[role].append(a)
+
+def make_salt():
+	return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def make_pw_hash(name, pw, salt = None):
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return "%s|%s" % (h,salt)
 
 class MainHandler(webapp2.RequestHandler):
 	template_dir = os.path.join(os.path.dirname(__file__), 'pages')
@@ -77,58 +101,44 @@ class SignupPageHandler(MainHandler):
 		username=self.request.get("username")
 		password=self.request.get("password")
 		verify_password=self.request.get("verify")
-		
-		#verifica presenza username
 		username_error_sw = False
 		username_error=""
-		if username == "":
+		password_missing_error_sw = False
+		password_match_error_sw = False
+		username_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+		if username == "" or username_re.match(username) == None:
 			username_error = "That's not a valid username."
 			username_error_sw = True
-		#verifica correttezza username
-		username_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-		if username_re.match(username) == None:
-			username_error_sw = True
-		#verifica presenza password
-		password_missing_error_sw = False
 		if password == "":
 			password_missing_error_sw = True
-		#verifica correttezza password
 		password_re = re.compile(r"^.{3,20}$")
 		if password_re.match(password) == None:
 			password_missing_error_sw = True
-		#verifica consistenza password
-		password_match_error_sw = False
 		if password != verify_password:
 			password_match_error_sw = True
 		if password_match_error_sw or username_error_sw or password_missing_error_sw:
 			self.write_signup(username,
-						email,
 						username_error,
 						password_missing_error_sw,
 						password_match_error_sw,
 						)
 		else:
-			#~ ck = ndb.GqlQuery("SELECT * FROM People WHERE username = :1",username)
-			ck = People.query(People.username == username)
-			if not ck.get():
-				u = People(parent = user_key())
-				u.username = username
-				u.password = make_pw_hash(username, password)
-				if email != "":
-					u.email = email
-				k = u.put()
-				self.response.headers.add_header('Set-Cookie',
-								'schooltagging=%s|%s; Path=/' % (str(k.id()),u.password))
-				self.redirect("/")
-			else:
+			if username in all_usernames():
 				username_error = "That user already exists"
 				self.write_signup(username,
-						email,
 						username_error,
 						password_missing_error_sw,
 						password_match_error_sw,
 						mail_error_sw,
 						)
+			else:
+				role = self.request.get("role")
+				password = make_pw_hash(username, password)
+				add_user(role, username, password)
+				self.response.headers.add_header('Set-Cookie',
+								'schooltagging=%s|%s; Path=/' % (str(username),str(password)))
+				self.redirect("/")
+				
 
 
 app = webapp2.WSGIApplication([
