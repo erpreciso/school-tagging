c5863884a3cfa6f93948f6caf9dbdd222c0f7d6d
erpreciso <trepreciso@gmail.com>
2014-11-30 10:11:45 +0100

    Create bot to test simultaneous datastore write.
    -add bot.sh with two wget calls
    -add two cookies for the two students

---
+++ src/bot.sh
@@ -0,0 +1,3 @@
+wget -a log.txt  -S --load-cookies annacookie.txt --post-data 'sessionID=5840605766746112&answer=Noun' http://localhost:8080/data/answer
+wget -a log.txt  -S --load-cookies pippocookie.txt --post-data 'sessionID=5840605766746112&answer=Article' http://localhost:8080/data/answer
+
